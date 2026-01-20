f7e88daa8f2c4f957cf5e36b25bf307ab18a3a14
Rushil Gowda <rushilgowdah@gamil.com>
2026-01-20 13:49:22 +0530

    Fix user model: id PK, email unique

--- app/database/models.py
+++ app/database/models.py
@@ -8,6 +8,6 @@ from sqlmodel import Field, SQLModel
 class User(SQLModel, table = True):
     
     id: int = Field(default=None, primary_key=True)
-    name: str
-    email: EmailStr
+    name: str 
+    email: EmailStr = Field(unique=True)
     password_hash: str
